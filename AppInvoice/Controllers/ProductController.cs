939ae08c9e70147010ff95e8f8a48b504bbdb495
Sebastián <sebas14121999@gmail.com>
2020-07-13 10:07:31 -0500

    vistas principales

---
+++ AppInvoice/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using AppInvoice.Models;
+
+namespace AppInvoice.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            IEnumerable<Product> product = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44330/api/");
+                var responseTask = client.GetAsync("product/get");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readJob = result.Content.ReadAsAsync<IList<Product>>();
+                    readJob.Wait();
+                    product = readJob.Result;
+                }
+                else
+                {
+                    product = Enumerable.Empty<Product>();
+                    ModelState.AddModelError(string.Empty, "Server error");
+                }
+            }
+            return View(product);
+        }
+    }
+}
---
+++ AppInvoice/Models/Response/ProductResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppInvoice.Models.Response
+{
+    public class ProductResponse
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+    }
+}
