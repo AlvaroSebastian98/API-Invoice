f0952db8cd26109e5808585f679fb9dbec23ade5
Sebastián <sebas14121999@gmail.com>
2020-07-13 13:35:08 -0500

    avance de vistas

--- AppInvoice/Controllers/ProductController.cs
+++ AppInvoice/Controllers/ProductController.cs
@@ -13,27 +13,29 @@ namespace AppInvoice.Controllers
         // GET: Product
         public ActionResult Index()
         {
-            IEnumerable<Product> product = null;
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri("https://localhost:44330/api/");
-                var responseTask = client.GetAsync("product/get");
-                responseTask.Wait();
+            //IEnumerable<Product> product = null;
+            //using (var client = new HttpClient())
+            //{
+            //    client.BaseAddress = new Uri("https://localhost:44330/api/");
+            //    var responseTask = client.GetAsync("product/get");
+            //    responseTask.Wait();
 
-                var result = responseTask.Result;
-                if (result.IsSuccessStatusCode)
-                {
-                    var readJob = result.Content.ReadAsAsync<IList<Product>>();
-                    readJob.Wait();
-                    product = readJob.Result;
-                }
-                else
-                {
-                    product = Enumerable.Empty<Product>();
-                    ModelState.AddModelError(string.Empty, "Server error");
-                }
-            }
-            return View(product);
+            //    var result = responseTask.Result;
+            //    if (result.IsSuccessStatusCode)
+            //    {
+            //        var readJob = result.Content.ReadAsAsync<IList<Product>>();
+            //        readJob.Wait();
+            //        product = readJob.Result;
+            //    }
+            //    else
+            //    {
+            //        product = Enumerable.Empty<Product>();
+            //        ModelState.AddModelError(string.Empty, "Server error");
+            //    }
+            //}
+            //return View(product);
+
+            return View();
         }
     }
 }
